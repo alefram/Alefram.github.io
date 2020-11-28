cb8d8115fa8c99abb2577aefc95a84b765d6d52d
alex <alexis@bloomcker.io>
2020-11-27 23:03:56 -0400

    add moon and other stuff

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,25 @@ export default function Home() {
         <Navbar/>     
       </nav>
       
-      <main>
+      <main className="mx-2">
 
-        <h1 className="text-4xl mt-20 ml-4 text-gray-800"> Who I am ?</h1>
+        <div className='text-center mt-10 mb-10'>
+          <h1 className="text-4xl ml-4 mb-2 text-gray-800"> Who I am ?</h1>
 
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A inventore voluptate explicabo voluptatibus, dolore reiciendis tempore totam eveniet assumenda sint in ipsam blanditiis porro fuga harum rerum neque id tempora!</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A inventore voluptate explicabo voluptatibus, dolore reiciendis tempore totam eveniet assumenda sint in ipsam blanditiis porro fuga harum rerum neque id tempora!</p>
+        </div>
+
+        <div>
+          <h1>Education</h1>
+
+          <ul>
+            <li>Electrical engineer student at Central University of Venezuela</li>
+            <li></li>
+          </ul>
+
+          <h1>Interested</h1>
+        </div>
+
         
         
       </main>
