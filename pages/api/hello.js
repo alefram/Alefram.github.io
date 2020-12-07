0d13b89c0679de84f86dbc36d9eaaff2447cc462
alex <cuatroalejandro@gmail.com>
2020-12-06 23:56:20 -0400

    add some style and modifications to the navbar

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,3 +4,4 @@ export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
+k
--- pages/index.js
+++ pages/index.js
@@ -15,24 +15,37 @@ export default function Home() {
       
       <main className="mx-2">
 
-        <div className='text-center mt-10 mb-10'>
-          <h1 className="text-4xl ml-4 mb-2 text-gray-800"> Who I am ?</h1>
+        <div className='text-center mt-6 mb-10'>
+          <h1 className="text-4xl ml-4 mb-2 font-semibold text-gray-900"> Who I am ?</h1>
 
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A inventore voluptate explicabo voluptatibus, dolore reiciendis tempore totam eveniet assumenda sint in ipsam blanditiis porro fuga harum rerum neque id tempora!</p>
+          <p className="text-gray-900 text-justify mx-3">
+            I am Electrical Engineer Student at Central University of Venezuela, also software developer Interested in machine learning and robotics for aerospace and medical applications. Currently, i am working on my Bachelor thesis that is about a collection of Reinforcement Learning enviroments for a robot arm in different tasks.
+          </p>
         </div>
 
         <div>
-          <h1>Education</h1>
 
-          <ul>
-            <li>Electrical engineer student at Central University of Venezuela</li>
-            <li></li>
-          </ul>
+          <div>
+            <h1 className=" text-2xl font-medium text-blue-800">Education</h1>
+            <div className="ml-4 text-gray-900">
+              <ul>
+                <li>Electrical engineer student at Central University of Venezuela</li>
+              </ul>
+            </div>
+          </div>
 
-          <h1>Interested</h1>
-        </div>
+          <div className="mt-2">
+            <h1 className=" text-2xl font-medium text-blue-800">Interested</h1>
+            <div className="ml-4 text-gray-900">
+              <ul>
+                <li>Tiny Machine Learning</li>
+                <li>Reinforcement Learning</li>
+                <li>Robotics</li>
+              </ul>
+            </div>
+          </div>
 
-        
+        </div>     
         
       </main>
 
