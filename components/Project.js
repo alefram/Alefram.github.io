b3886bb338262435a3a64114553c176f885f8504
alex <cuatroalejandro@gmail.com>
2020-12-16 23:51:41 -0400

    update color to Post and Project

--- components/Project.js
+++ components/Project.js
@@ -5,7 +5,7 @@ function Project({description,title,id}) {
         <div>
             <div className="p-2 flex flex-col">
                 <Link href={`/projects/${id}`}>
-                    <a className="text-lg  font-medium">{title}</a>
+                    <a className="text-lg  font-medium text-gray-900">{title}</a>
                 </Link>
                 <small className="mt-1 text-gray-500">
                     {description}
