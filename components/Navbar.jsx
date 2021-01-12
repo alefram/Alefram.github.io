6e276996270149c932b58fa38d8557fe7223c5f1
alex <cuatroalejandro@gmail.com>
2021-01-12 19:03:52 -0400

    more fixes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,30 +1,19 @@
 import Link from 'next/link'
-// import Image from 'next/image'
 
 
 function Navbar() {
 
   return (
     <>
-      <nav className="flex p-4 container mx-auto lg:max-w-screen-md justify-between">
+      <nav className="flex flex-col md:flex-row p-4 container mx-auto lg:max-w-screen-md justify-between">
         <div className="mt-1 pr-4 py-4">
           <Link href="/">
             <a className="text-titles text-2xl font-Alegreya font-bold italic ">
-              Alexis Fraudita
-              {/* <Image
-                src="/yo.jpeg"
-                width={100}
-                height={100}
-                className=" rounded-full"
-              /> */}
+              Ale-Ale Blog
             </a> 
           </Link>
         </div>
-        {/* <div>
-          <h1 className=" text-titles">Alexis Fraudita</h1>
-        </div> */}
-
-        <div className="m-2 p-4 space-x-4 text-center">
+        <div className=" md:m-2 md:p-4 space-x-4 md:text-center">
           <Link  href="/Posts">
             <a className="text-gray-50 font-Roboto font-normal hover:underline hover:text-titles">Posts</a>
           </Link>
