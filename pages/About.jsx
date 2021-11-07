9f035633872076255e9b3e7f55a1b67ec735427b
alex <cuatroalejandro@gmail.com>
2021-11-06 21:51:45 -0400

    add me image

--- pages/About.jsx
+++ pages/About.jsx
@@ -27,12 +27,17 @@ const About = () => {
                         </h1>
                     </div>
                     <div className="">
+                        <img src="/images/me.jpg" className="mx-auto w-40 rounded-xl p-2 mt:m-4" alt=""/>
+
                         <div>
-                            <p className=" md:mx-auto prose md:text-justify text-gray-50  font-nunito clear-left">
+                            <p className="md:mx-auto prose md:text-justify text-gray-50  font-nunito clear-left">
                                 I am Electrical Engineer Student at Central University of Venezuela, also software developer interested in AI techniques specially in reinforcement learning and Federated Learning for control problems in robotics and embedded computers. Currently, i am working on my Bachelor thesis that is about a framework for create smart controllers in robots arms using Reinforcement Learning i hope in future do some tutorials about what i am learning in this area.
                             </p>
-                            <p className=" md:mx-auto prose md:text-justify font-nunito text-gray-50 mt-2">
-                                I love technology in different areas and how it can be integrated for resolve real problems that people needs. I want to contribute in research in robotics for different kind of applications where the robot help humans to be better and not only take their jobs. I like sports like table tennis, compose music and watch movies that for me is a source of inspiration.
+                            <p className="md:mx-auto prose md:text-justify font-nunito text-gray-50 mt-2">
+                                I love technology in different areas and how it can be integrated for resolve real problems that people needs. I want to contribute in research in robotics for different kind of applications where the robot help humans to be better and not only take their jobs.
+                            </p>
+                            <p className="md:mx-auto prose md:text-justify font-nunito text-gray-50 mt-2">
+                                Some others things and interests that i have are sports like table tennis, compose music and watch movies that for me is a source of inspiration.  
                             </p>
                         </div>
                     </div>
