c567b7b745cada7d8e8963da179e8bda5d89edce
alex <cuatroalejandro@gmail.com>
2021-11-07 15:59:29 -0400

    update projects

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -6,36 +6,6 @@ import { getSortedProjectsData } from '../lib/projects';
 
 export default function Projects({allProjectsData}) {
 
-    if (allProjectsData.length === 0) {
-        return (
-            <div className="bg-background min-h-screen">
-                <Head>
-                    <title>My projects</title>
-                    <link rel="icon" href="/favicon.ico" />
-                    <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
-                    <style data-href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"/>
-                    <style data-href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet"/>
-                    <style data-href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet"/>
-                </Head>
-    
-                <Navbar/>
-    
-                <main className="mx-auto px-5 lg:max-w-screen-md">
-                    <div className="m-2 p-4">
-                        <h1 className="prose text-5xl text-center text-gray-100 font-bebasNue">Projects</h1>
-                    </div>
-                    <div>
-                        <h1 className="text-2xl text-center text-gray-100 font-bold">
-                            There is no projects for now :(
-                        </h1>
-                    </div>
-                </main>
-                <Footer/>
-    
-            </div>
-        );
-    }
-
     return (
         <div className="bg-background min-h-screen"> 
             <Head>
@@ -54,6 +24,11 @@ export default function Projects({allProjectsData}) {
                     <h1 className=" prose text-5xl text-center text-gray-100 font-bebasNue">Projects</h1>
                 </div>
                 <div>
+                    {allProjectsData.length === 0 && (
+                        <h1 className="text-2xl text-center text-gray-100 font-bold">
+                            There is no projects for now :(
+                        </h1>
+                    )}
                     <ul>
                         {allProjectsData.map(({ id, date, title }) => (
                             <li key={id}>
