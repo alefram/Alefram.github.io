4d2ed0c7cc01b1d5644466dc09db5c56c977824e
alex <cuatroalejandro@gmail.com>
2022-04-05 09:38:12 -0400

    update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,20 +7,17 @@ const Navbar = () => {
 			<div className="flex flex-col md:flex-row p-4 container mx-auto lg:max-w-screen-md justify-between">
 				<div className="mt-1 pr-4 py-4">
 					<Link href="/">
-						<a className="text-gradient bg-gradient-to-r from-gray-700 to-supertext text-4xl font-semibold ">
+						<a className="text-gradient bg-gradient-to-r from-gray-700 to-supertext text-4xl font-bold ">
 							Alexis Fraudita 
 						</a>
 					</Link>
 				</div>
 				<div className=" md:m-2 md:p-4 space-x-4 md:text-center">
 					<Link href="/Posts">
-						<a className="text-background font-semibold font-Roboto hover:underline hover:text-blue-500">Posts</a>
+						<a className="text-background font-bold font-nunito hover:underline hover:text-blue-500">Posts</a>
 					</Link>
 					<Link href="/Projects">
-						<a className="text-background font-semibold font-Roboto hover:underline hover:text-blue-500">Projects</a>
-					</Link>
-					<Link href="/About">
-						<a className="text-background font-semibold font-Roboto hover:underline hover:text-blue-500">About</a>
+						<a className="text-background font-bold font-nunito hover:underline hover:text-blue-500">Projects</a>
 					</Link>
 				</div>
 			</div>
