59136fbe6b5afa6a0f1e8d3aec2be946ab6f282b
alefram <fraumalex@gmail.com>
2023-06-01 10:36:10 -0400

    fix mobile version of post component

--- components/Post.jsx
+++ components/Post.jsx
@@ -6,19 +6,23 @@ const Post = ({ post }) => {
     return (
         <>
             <div className="flex justify-between">
-                <Link href={`/posts/${post.id}`}>
-                    <a 
-                        className=" font-bold font-nunito 
-                     text-blue-900 text-opacity-90 text-lg lg:text-xl">
-                        {post.title}
-                    </a>
-                </Link>
-                <small 
-                    className="mt-1 font-nunito text-gray-700 text-sm 
-                        lg:text-base"
-                >
-                    <Date dateString={post.date}/>
-                </small>
+                <div>
+                    <Link href={`/posts/${post.id}`}>
+                        <a 
+                            className="font-bold font-nunito 
+                        text-blue-900 text-opacity-90 text-lg lg:text-xl">
+                            {post.title}
+                        </a>
+                    </Link>
+                </div>
+                <div className='justify-end whitespace-nowrap'>
+                    <small 
+                        className="mt-1 font-nunito text-gray-700 text-sm 
+                            lg:text-base"
+                    >
+                        <Date dateString={post.date}/>
+                    </small>
+                </div>
             </div>
         </>
     );
