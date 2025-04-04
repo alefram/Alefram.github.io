270169d8bc6f3a0c83e5083553e30261599ea161
Alexis <fraumalex@gmail.com>
2025-04-04 18:16:18 -0400

    test git

--- pages/About.jsx
+++ pages/About.jsx
@@ -67,8 +67,8 @@ const About = () => {
                     <p className="mt-3 font-Roboto text-neutral-800 dark:text-neutral-400">
                         Currently, I am exploring various topics that I am curious 
                         about, including control theory, 
-                        physics simulation, reinforcement learning, computer vision
-                        and embedded systems apply to robotics.
+                        physics simulation, reinforcement learning
+                        and electronics apply to robotics.
                     </p>
                     <p className='mt-3 font-Roboto text-neutral-800 dark:text-neutral-400'>
                         Previously held the position of Assistant Professor in 
